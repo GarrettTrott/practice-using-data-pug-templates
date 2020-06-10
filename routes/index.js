68177b0536dc90161defa5d2827a5479cc4061d0
Garrett Trott <garretttrott@gmail.com>
2020-06-10 00:10:31 -0700

    add recipes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { recipes } = require('../data/data.json');
+const { recipes } = require("../data/data.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // 1. Pass all recipe data to 'index' template
-  res.render('index');
+  res.render("index", { recipes });
 });
 
 /* GET recipe page. */
-router.get('/recipes/:id', function(req, res, next) {
+router.get("/recipes/:id", function (req, res, next) {
   const recipeId = req.params.id;
-  const recipe = recipes.find( ({ id }) => id === +recipeId );
-  
+  const recipe = recipes.find(({ id }) => id === +recipeId);
+
   if (recipe) {
     // 2. Pass the recipe data to the 'recipe' template
-    res.render('recipe');
+    res.render("recipe", { recipe });
   } else {
     res.sendStatus(404);
   }
